b412b486a243dfa90bec1f9bb8ba9fbc68109ec8
Muxammad <muxammadcodes@gmail.com>
2023-04-20 14:30:33 +0500

    DataAccses layer added with seed datas

--- src/OnlineMarket.Web/Program.cs
+++ src/OnlineMarket.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineMarket.DataAccess.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<OnlineMarketDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
