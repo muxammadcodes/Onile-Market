91417c4dd0f85612c0b6e2ce2ed54f467b4dcab0
Muxammad <muxammadcodes@gmail.com>
2023-04-26 11:36:37 +0500

    fully done code

--- src/OnlineMarket.DataAccess/Migrations/20230426063004_seed.Designer.cs
+++ src/OnlineMarket.DataAccess/Migrations/20230426063004_seed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineMarket.DataAccess.Context;
 
@@ -11,9 +12,10 @@ using OnlineMarket.DataAccess.Context;
 namespace OnlineMarket.DataAccess.Migrations
 {
     [DbContext(typeof(OnlineMarketDbContext))]
-    partial class OnlineMarketDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230426063004_seed")]
+    partial class seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/OnlineMarket.DataAccess/Migrations/20230426063004_seed.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineMarket.DataAccess.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 26, 6, 30, 4, 153, DateTimeKind.Utc).AddTicks(6040));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 26, 6, 30, 4, 153, DateTimeKind.Utc).AddTicks(6044));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 26, 6, 30, 4, 153, DateTimeKind.Utc).AddTicks(6045));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 26, 6, 30, 4, 153, DateTimeKind.Utc).AddTicks(6046));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 26, 6, 30, 4, 153, DateTimeKind.Utc).AddTicks(6048));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 24, 16, 51, 38, 882, DateTimeKind.Utc).AddTicks(3054));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 24, 16, 51, 38, 882, DateTimeKind.Utc).AddTicks(3059));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 24, 16, 51, 38, 882, DateTimeKind.Utc).AddTicks(3061));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 24, 16, 51, 38, 882, DateTimeKind.Utc).AddTicks(3062));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2023, 4, 24, 16, 51, 38, 882, DateTimeKind.Utc).AddTicks(3064));
+        }
+    }
+}
